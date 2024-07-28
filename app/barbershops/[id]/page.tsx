1698583e0c2cf859cd1f65d87f6f9a451cdc29cf
Fernandasacosta <fernandasacostaa@gmail.com>
2024-07-28 15:12:41 -0300

    feat: add ServiceItem component

--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -2,6 +2,8 @@ import { Button } from "@/app/_components/ui/button";
 import { db } from "@/app/_lib/prisma";
 import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from "lucide-react";
 import Image from "next/image";
+import BarbershopInfo from "./_components/barbershop-info";
+import ServiceItem from "./_components/service-item";
 
 interface BarbershopDetailsPage {
   params: {
@@ -19,6 +21,9 @@ const BarbershopDetailsPage = async ({ params }: BarbershopDetailsPage) => {
     where: {
       id: params.id,
     },
+    include: {
+      services:true,
+    }
   });
 
   if(!barbershop ){
@@ -26,45 +31,19 @@ const BarbershopDetailsPage = async ({ params }: BarbershopDetailsPage) => {
     return null;
   }
 
-  return ( 
-    <div>
-      <div className="h-[250px] w-full relative">
-        <Button size="icon" variant="outline" className="z-50 top-4 left-4 absolute">
-          <ChevronLeftIcon/>
-        </Button>
-
-        <Button size="icon" variant="outline" className="z-50 top-4 right-4 absolute">
-          <MenuIcon/>
-        </Button>
-
-        <Image 
-          src={barbershop.imageUrl} 
-          fill 
-          alt={barbershop.name} 
-          style={{
-            objectFit:"cover",
-          }}
-          className="opacity-75"
-        />
-      </div>
-
-      <div className="px-5 pt-3 pb-6 border-b border-solid border-secondary">
-        <h1 className="text-xl font-bold">{barbershop.name}</h1>
-
-        <div className="flex items-center gap-1 mt-2">
-          <MapPinIcon className="text-primary" size={18}/>
-          <p className="text-sm">{barbershop.address}</p>
-        </div>
-
-        <div className="flex items-center gap-1 mt-2">
-          <StarIcon className="text-primary" size={18}/>
-          <p className="text-sm">5,0 (345 avaliações)</p>
-        </div>
-
-      </div>
-
-    </div>
-   );
-}
+  return (
+  
+  <div>
+    <BarbershopInfo barbershop={barbershop}/>
+   
+   <div className="px-5 flex flex-col gap-4 py-6">
+    {barbershop.services.map(service => (
+        <ServiceItem key={service.id} service={service}/>
+      ))}
+   </div>
+  </div>
+  
+  );
+};
  
 export default BarbershopDetailsPage;
