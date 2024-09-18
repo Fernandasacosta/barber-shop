93f1d74375dce0002d3bd5f5d983952527610be1
Fernandasacosta <fernandasacostaa@gmail.com>
2024-09-18 16:52:13 -0300

    feat: ensure cofirmation is shown on booking cancelation

--- app/bookings/page.tsx
+++ app/bookings/page.tsx
@@ -4,7 +4,6 @@ import { authOptions } from "../api/auth/[...nextauth]/route";
 import { redirect } from "next/navigation";
 import { db } from "../_lib/prisma";
 import BookingItem from "../_components/booking-item";
-import { isFuture, isPast } from "date-fns";
 
 const BookingsPage = async() => {
 
@@ -54,26 +53,30 @@ const BookingsPage = async() => {
     <Header/>
 
     <div className="py-6 px-5">
-      <h1 className="text-xl font-bold">Agendamentos</h1>
+      <h1 className="text-xl font-bold mb-6">Agendamentos</h1>
 
-      {confirmedBookings.length === 0 && finishedBookings.length === 0 && (
-        <h2 className="font-bold text-sm text-gray-400 uppercase mt-6 mb-3">Confirmados</h2>
-      )}
-
-       <div className="flex flex-col gap-3">
+      {confirmedBookings.length > 0 && (
+        <>
+         <h2 className="font-bold text-sm text-gray-400 uppercase mb-3">Confirmados</h2>
+         <div className="flex flex-col gap-3">
         {confirmedBookings.map((booking) => (
           <BookingItem key={booking.id} booking={booking} />
         ))}
        </div>
+        </>
+      )}
 
+       {finishedBookings.length > 0 && (
+        <>
+          <h2 className="font-bold text-sm text-gray-400 uppercase mt-6 mb-3">Finalizados</h2>
+          <div className="flex flex-col gap-3">
+          {finishedBookings.map((booking) => (
+            <BookingItem key={booking.id} booking={booking} />
+          ))}
+          </div>
+        </>
+       )}
        
-      <h2 className="font-bold text-sm text-gray-400 uppercase mt-6 mb-3">Finalizados</h2>
-
-        <div className="flex flex-col gap-3">
-        {finishedBookings.map((booking) => (
-          <BookingItem key={booking.id} booking={booking} />
-        ))}
-        </div>
       </div>
     </>
    );
