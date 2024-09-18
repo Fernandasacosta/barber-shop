859e059bba22c7dc1ea805562a5278f6af404ae8
Fernandasacosta <fernandasacostaa@gmail.com>
2024-09-18 08:03:48 -0300

    feat: ensure booking can be canceled

--- app/bookings/page.tsx
+++ app/bookings/page.tsx
@@ -56,7 +56,9 @@ const BookingsPage = async() => {
     <div className="py-6 px-5">
       <h1 className="text-xl font-bold">Agendamentos</h1>
 
-      <h2 className="font-bold text-sm text-gray-400 uppercase mt-6 mb-3">Confirmados</h2>
+      {confirmedBookings.length === 0 && finishedBookings.length === 0 && (
+        <h2 className="font-bold text-sm text-gray-400 uppercase mt-6 mb-3">Confirmados</h2>
+      )}
 
        <div className="flex flex-col gap-3">
         {confirmedBookings.map((booking) => (
