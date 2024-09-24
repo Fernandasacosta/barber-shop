baadabe4d4f458bddcfad7692174ded353d2e869
Fernandasacosta <fernandasacostaa@gmail.com>
2024-09-24 19:03:38 -0300

    feat: ensure semantic tags are used on footer and header

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -4,40 +4,32 @@ import Image from "next/image";
 import { Card, CardContent } from "./ui/card";
 import { Button } from "./ui/button";
 import { MenuIcon } from "lucide-react";
-import { signIn, signOut, useSession } from "next-auth/react";
 import { SheetContent, SheetTrigger, Sheet } from "./ui/sheet";
-
 import SideMenu from "./side-menu";
 import Link from "next/link";
 
 const Header = () => {
-  const { data, status } = useSession();
-
-  const handleLogoutClick = () => signOut();
-  const handleLoginClick = () => signIn("google");
-
   return ( 
+    <header>
     <Card>
-      <CardContent className="p-5 justify-between flex flex-row items-center">
+      <CardContent className="p-5 justify-between items-center flex flex-row">
         <Link href="/">
-      <Image src="/logo.png" alt="fsw Barber" height={22} width={120}/>
-      </Link>
-
-      <Sheet>
-        <SheetTrigger asChild>
-          <Button className="h-8 w-8" variant="outline" size="icon">
-            <MenuIcon size={16}/>
-          </Button> 
-        </SheetTrigger>
-
-        <SheetContent className="p-0">
-          <SideMenu/>
-        </SheetContent>
-      </Sheet>
-     
-      </CardContent>
-    </Card>
-   );
-}
+          <Image src="/logo.png" alt="FSW Barber" height={18} width={120} />
+        </Link>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button variant="outline" size="icon">
+              <MenuIcon size={16} />
+            </Button>
+          </SheetTrigger>
+          <SheetContent className="p-0">
+              <SideMenu />
+            </SheetContent>
+          </Sheet>
+        </CardContent>
+      </Card>
+    </header>
+  );
+};
  
 export default Header;
